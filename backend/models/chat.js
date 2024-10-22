016671f91abbd4e5cae28bf8ad7f151bfd329826
Stuckertks09 <stuckertks09@gmail.com>
2024-10-22 10:58:15 -0500

    Update chat.js

--- backend/models/chat.js
+++ backend/models/chat.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const chatSchema = new mongoose.Schema({
     chatId: { type: String, required: true },
     senderId: { type: String, required: true },
-    senderName: { type: String, required: false }
+    senderName: { type: String, required: false },
     message: { type: String, required: true },
     timestamp: { type: Date, default: Date.now }
 });
