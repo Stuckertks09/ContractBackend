7ad2e7bc3126a487663202b07135001c6c81eb98
Stuckertks09 <stuckertks09@gmail.com>
2024-10-22 10:16:23 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const http = require('http');  // Required for integrating Socket.IO with Express
-const socketIo = require('socket.io');  // For WebSockets (Socket.IO)
+const http = require('http');
+const socketIo = require('socket.io');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
@@ -11,43 +11,40 @@ const searchRoute = require('./routes/search');
 const authRoute = require('./routes/auth');
 const showingRoutes = require('./routes/Showing');
 const userRoutes = require('./routes/user');
-const chatRoutes = require('./routes/chatRoutes')
-
+const chatRoutes = require('./routes/chatRoutes');
 
 dotenv.config(); // Load environment variables
 
 const app = express();
-
-// Create HTTP server and integrate with Socket.IO
-const server = http.createServer(app);
-const io = socketIo(server);  // Socket.IO instance on the server
+const server = http.createServer(app);  // Create HTTP server for Express and Socket.IO
+const io = socketIo(server);  // Attach Socket.IO to the HTTP server
 
 // Twilio client setup
 const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 // Set up CORS
 app.use(cors({
-    origin: '*',  // Or specify your app's IP or domain
+    origin: '*',  // Allow all origins or specify your app's IP/domain
     methods: ['GET', 'POST'],
     credentials: true
-  }));
+}));
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-// Connect to MongoDB using the URI from the .env file
+// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch((error) => console.error('Error connecting to MongoDB:', error));
 
-// Use listings API route
+// Use API routes
 app.use('/api/listings', listingsRoute);
 app.use('/api/send-message', twilioRoute);
 app.use('/api/search', searchRoute);
 app.use('/api/auth', authRoute);
 app.use('/api', showingRoutes);
 app.use('/api', userRoutes);
-app.use('/api/chat', chatRoutes); 
+app.use('/api/chat', chatRoutes);
 
 // Import and use the Socket.IO handler
 require('./routes/socket')(io);  // Pass the io instance to the socket handler
