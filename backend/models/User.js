2027a728363a321a109fa79ebca201f74f4b264b
Stuckertks09 <stuckertks09@gmail.com>
2024-10-22 10:44:10 -0500

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,9 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, trim: true },
   email: { type: String, required: true, unique: true, trim: true },
   password: { type: String, required: true },
-  chatId: { type: String, required: true }
+  chatId: { type: String, required: true },
+  FirstName: { type: String, required: false },
+  LastName: { type: String, required: false }
 });
 
 // Create the User model
