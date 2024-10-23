26e35dde07f826e00d558ecb418c952aa4837930
Stuckertks09 <stuckertks09@gmail.com>
2024-10-23 13:22:35 -0500

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,12 +1,12 @@
-// routes/user.js
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const Agent = require('../models/Agent');  // Import the Agent model
 
 // Route to get a list of users
 router.get('/users', async (req, res) => {
   try {
-    // Find all users and return only necessary fields (e.g., username, _id)
+    // Find all users and return only necessary fields (e.g., username, email, _id)
     const users = await User.find({}, 'username email _id');
     res.status(200).json(users);
   } catch (error) {
@@ -15,15 +15,15 @@ router.get('/users', async (req, res) => {
   }
 });
 
-// Route to get a list of users
+// Route to get a list of agents
 router.get('/agents', async (req, res) => {
   try {
-    // Find all users and return only necessary fields (e.g., username, _id)
-    const users = await User.find({}, 'username email _id');
-    res.status(200).json(users);
+    // Find all agents and return only necessary fields (e.g., firstName, lastName, email, _id)
+    const agents = await Agent.find({}, 'firstName lastName email _id');
+    res.status(200).json(agents);
   } catch (error) {
-    console.error('Error fetching users:', error);
-    res.status(500).json({ message: 'Error fetching users' });
+    console.error('Error fetching agents:', error);
+    res.status(500).json({ message: 'Error fetching agents' });
   }
 });
 
